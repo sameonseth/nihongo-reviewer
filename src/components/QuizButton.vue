39a2906916d70817c9522198099b41e7138b881b
sameonseth <sethlabelsameon@gmail.com>
2024-01-28 20:40:08 +0800

    modified quizzes

--- src/components/QuizButton.vue
+++ src/components/QuizButton.vue
@@ -39,24 +39,13 @@
     v-for="(quizCode, index) in n4KanjiCodeQuizArray"
     :key="quizCode"
     >
-        <div v-if="index <= 19">
-            <button
-            type="button"
-            class="kanji-option"
-            @click.prevent="setQuiz(quizCode)"
-            >
-            {{ n4FirstName + (index+1) + kanjiOptionLastName }}
-            </button>
-        </div>
-        <div v-else-if="index === 20">
-            <button
-            type="button"
-            class="kanji-option"
-            @click.prevent="setQuiz('LastKQuiz')"
-            >
-            Last Kanji Quiz
-            </button>
-        </div>
+        <button
+        type="button"
+        class="kanji-option"
+        @click.prevent="setQuiz(quizCode)"
+        >
+        {{ n4FirstName + (index+1) + kanjiOptionLastName }}
+        </button>
     </div>
 </template>
 <script>
